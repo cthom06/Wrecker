e954e29917fd989ecd9b7039b3c122f60f1d01dd
Corey Thomasson <cthom06@users.noreply.github.com>
2016-01-24 23:24:52 -0500

    start of wrecker

---
+++ Wrecker/Routing.fs
@@ -0,0 +1,59 @@
+﻿module Cthom06.Wrecker.Routing
+
+open Cthom06.Wrecker.Http
+open System.Text.RegularExpressions
+
+let orDef def opt =
+    match opt with
+    | Some v -> v
+    | None -> def
+
+let try500 f (req : Request) =
+    try
+        f req
+    with
+    | e ->
+        let body = "500 - An error occurred"
+        { Code = 500
+          Reason = "Server Error"
+          Version = req.Version
+          Header = Map.empty
+          Body = Body.fromString body }
+
+let or404 f (req : Request) =
+    f req
+    |> orDef
+        { Code = 404
+          Reason = "Not Found"
+          Version = req.Version
+          Header = Map.empty
+          Body = Body.fromString "404 - Resource not found" }
+
+let onlyGet f (req : Request) =
+    if req.Method = "GET" then
+        Some (f req)
+    else
+        None
+
+let onlyPost f (req : Request) =
+    if req.Method = "POST" then
+        Some (f req)
+    else
+        None
+
+let plexMethod handlers (req : Request) =
+    Map.tryFind req.Method handlers
+    |> Option.map (fun f -> f req)
+
+let plexHost handlers (req : Request) =
+    Map.tryFind req.Url.Host handlers
+    |> Option.map (fun f -> f req)
+
+let plexPathPrefix handlers (req : Request) =
+    List.tryFind (fst >> req.Url.AbsolutePath.StartsWith) handlers
+    |> Option.map (fun (_,f) -> f req)
+
+let plexPathRegex (handlers : (Regex * _) list) (req : Request) =
+    handlers
+    |> List.tryFind (fun (pattern,_) -> (pattern.Match req.Url.AbsolutePath).Success)
+    |> Option.map (fun (path,f) -> f req)
---
+++ Wrecker/Util.fs
@@ -0,0 +1,80 @@
+﻿module Cthom06.Wrecker.Util
+
+open Cthom06.Wrecker.Http
+open Cthom06.Wrecker.Routing
+open System.IO
+open System.IO.Compression
+
+let defaultMimeTypes =
+    [ ".html", "text/html; charset=utf-8"
+      ".css", "text/css; charset=utf-8"
+      ".js", "application/x-javascript"
+      ".txt", "text/plain; charset=utf-8"
+      ".gif", "image/gif"
+      ".jpg", "image/jpeg"
+      ".png", "image/png" ]
+    |> Map.ofList
+
+let private getStaticFileHeaders (stat : FileInfo) =
+    let fileType =
+        match Map.tryFind (Path.GetExtension stat.FullName) defaultMimeTypes with
+        | Some mime -> mime
+        | None -> "application/octet-stream"
+    [ "Content-Type", [fileType]
+      "Content-Length", [stat.Length.ToString ()]
+      "Last-Modified", [stat.LastWriteTimeUtc.ToString "R"] ]
+    |> Map.ofList
+
+let private fileResp stat (req : Request) =
+    let headers = getStaticFileHeaders stat
+    let body =
+        match req.Method with
+        | "HEAD" -> ByteBody [||]
+        | "GET" -> StreamBody (stat.OpenRead ())
+        | _ -> raise <| new System.InvalidOperationException ("Non head/get request passed to static handler")
+    { Code = 200
+      Reason = "OK"
+      Version = req.Version
+      Header = headers
+      Body = body }
+
+let private unmodResp (req : Request) =
+    { Code = 304
+      Reason = "Not Modified"
+      Version = req.Version
+      Header = Map.empty
+      Body = EmptyBody }
+
+let rec doFile (req : Request) path =
+    try
+        let stat = new FileInfo (path)
+        if stat.Exists then
+            match Map.tryFind "If-Modified-Since" req.Header with
+            | Some (d::_) ->
+                let dr = ref System.DateTime.MinValue
+                if System.DateTime.TryParse (d, dr) && !dr <= stat.LastAccessTimeUtc then
+                    Some (unmodResp req)
+                else
+                    Some (fileResp stat req)
+                        
+            | _ ->
+                Some (fileResp stat req)
+        else
+            tryIndex req path
+    with
+    | :? FileNotFoundException -> None
+
+and tryIndex req path =
+    let stat = new DirectoryInfo (path)
+    if stat.Exists then
+        Path.Combine (path, "index.html")
+        |> doFile req
+    else
+        None
+
+let staticFileHandler prefix folder (req : Request) =
+    if req.Url.AbsolutePath.StartsWith prefix then
+        let path = Path.Combine (folder, req.Url.AbsolutePath.Substring prefix.Length)
+        doFile req path
+    else
+        None
